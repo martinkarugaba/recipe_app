2c1b6d80ad78622453ac1019c4aaeef9c14a999d
Martin Karugaba <martinkarugaba21@gmail.com>
2024-01-11 22:24:14 +0300

    Add recipe_foods table

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,4 +1,8 @@
 class RecipeFood < ApplicationRecord
   belongs_to :recipe
   belongs_to :food
+
+  def total_price
+    recipe_foods.sum { |recipe_food| recipe_food.food.price }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,6 @@ Rails.application.routes.draw do
   resources :recipes do
     put 'toggle_public', on: :member
     get 'public_recipes', on: :collection
-    get 'new_recipe_food', on: :member
-    post 'create_new_recipe_food', on: :member
     post 'generate_shopping_list', on: :member
 
     resources :recipe_foods
