3efd89f7e76f6ee4aaa6de8aedef145579d16c58
mryeminthu <dhammaarchive@hotmail.com>
2024-01-11 18:54:44 +0630

    Fix linter error

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  describe 'Validations' do
-    let(:user_instance) { User.create(email: 'user_test@example.com', password: 'user_password') }
+  let(:user_instance) { User.create(email: 'user_test@example.com', password: 'user_password') }
 
+  describe 'Validations' do
     context 'with valid attributes' do
       it 'is valid' do
         recipe = Recipe.new(
-          name: 'Delicious Dish',
+          name: 'Sample Recipe',
           preparation_time: '25 mins',
           cooking_time: '40 mins',
           description: 'A tasty dish description',
@@ -18,48 +18,28 @@ RSpec.describe Recipe, type: :model do
     end
 
     context 'with invalid attributes' do
+      before do
+        @invalid_recipe = Recipe.new(user: user_instance)
+      end
+
       it 'is not valid without a name' do
-        recipe = Recipe.new(
-          preparation_time: '25 mins',
-          cooking_time: '40 mins',
-          description: 'A tasty dish description',
-          user: user_instance
-        )
-        expect(recipe).to_not be_valid
-        expect(recipe.errors[:name]).to include("can't be blank")
+        expect(@invalid_recipe).to_not be_valid
+        expect(@invalid_recipe.errors[:name]).to include("can't be blank")
       end
 
       it 'is not valid without preparation_time' do
-        recipe = Recipe.new(
-          name: 'Delicious Dish',
-          cooking_time: '40 mins',
-          description: 'A tasty dish description',
-          user: user_instance
-        )
-        expect(recipe).to_not be_valid
-        expect(recipe.errors[:preparation_time]).to include("can't be blank")
+        expect(@invalid_recipe).to_not be_valid
+        expect(@invalid_recipe.errors[:preparation_time]).to include("can't be blank")
       end
 
       it 'is not valid without cooking_time' do
-        recipe = Recipe.new(
-          name: 'Delicious Dish',
-          preparation_time: '25 mins',
-          description: 'A tasty dish description',
-          user: user_instance
-        )
-        expect(recipe).to_not be_valid
-        expect(recipe.errors[:cooking_time]).to include("can't be blank")
+        expect(@invalid_recipe).to_not be_valid
+        expect(@invalid_recipe.errors[:cooking_time]).to include("can't be blank")
       end
 
       it 'is not valid without a description' do
-        recipe = Recipe.new(
-          name: 'Delicious Dish',
-          preparation_time: '25 mins',
-          cooking_time: '40 mins',
-          user: user_instance
-        )
-        expect(recipe).to_not be_valid
-        expect(recipe.errors[:description]).to include("can't be blank")
+        expect(@invalid_recipe).to_not be_valid
+        expect(@invalid_recipe.errors[:description]).to include("can't be blank")
       end
     end
   end
