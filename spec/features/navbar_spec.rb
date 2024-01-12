6e45817d09c8f03b8c963b52fffd201b0abd4f95
mryeminthu <dhammaarchive@hotmail.com>
2024-01-12 12:50:14 +0630

    Add test for navbar

---
+++ spec/features/navbar_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.feature 'Navbar', type: :feature do
+  context 'when user is signed out' do
+    scenario 'displays the correct navbar links' do
+      visit root_path
+
+      expect(page).to have_link('Recipe App', href: '#')
+      expect(page).to have_link('Add Food', href: '/foods')
+      expect(page).to have_link('Recipes List', href: '/recipes')
+      expect(page).to have_link('Public Recipes', href: '#')
+      expect(page).to have_link('Shopping List', href: '#')
+
+      expect(page).to have_button('Sign in')
+      expect(page).to have_link('Sign up')
+      expect(page).not_to have_button('Sign out')
+    end
+  end
+end
