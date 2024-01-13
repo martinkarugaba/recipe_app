00dbd68a78eb79bf3a7bcba3797c2e4df4db0b40
Martin Karugaba <martinkarugaba21@gmail.com>
2024-01-13 13:51:24 +0300

    Fix shopping list link

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,28 +1,25 @@
-# app/controllers/shopping_list_controller.rb
-
 class ShoppingListController < ApplicationController
   before_action :authenticate_user!
 
   def index
     @user = current_user
-    @shopping_list_items = generate_shopping_list(@user)
-  end
-
-  def generate_shopping_list
-    @recipe = Recipe.find(params[:id])
-    @user = current_user
-    @shopping_list_items = generate_shopping_list_items(@user, @recipe)
-
-    render 'shopping_list/index'
+    @recipe = Recipe.first
+    @shopping_list_items = generate_shopping_list(@user, @recipe)
   end
 
   private
+
+  def generate_shopping_list(user, recipe)
+    @user = user
+    @recipe = recipe
+    @shopping_list_items = generate_shopping_list_items(@user, @recipe)
+    render 'shopping_list/index'
+  end
 
   def generate_shopping_list_items(user, recipe)
     missing_food = []
 
     recipe.foods.each do |food|
-      # Check if the user already has the food in their general food list
       missing_food << food unless user.foods.exists?(id: food.id)
     end
 
